5ede55f72a4c8acce4e898d1f2615508be7ab4a1
owen590 <145735121+owen590@users.noreply.github.com>
2025-12-13 21:12:31 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.tiantianxiangshang',
+  appName: '天天向上',
+  webDir: 'dist'
+};
+
+export default config;
